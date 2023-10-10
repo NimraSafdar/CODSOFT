11ded8324aabb0d949700cb2534746c37ef94a5b
Nimra Safdar <nimrasafdar2001@gmail.com>
2023-10-10 09:10:21 +0500

    Create Calculator.py

---
+++ Task 2/Calculator.py
@@ -0,0 +1,40 @@
+def addition(number1, number2):
+    answer = number1 + number2
+    return answer
+
+def subtraction(number1, number2):
+    answer = number1 - number2
+    return answer
+
+def multiplication(number1, number2):
+    answer = number1 * number2
+    return answer
+
+def division(number1, number2, division_type):
+    try:
+        if division_type.upper() == "INT":
+            answer = number1 // number2
+        elif division_type.upper() == "FLOOR":
+            answer = number1 / number2
+        return answer
+    except ZeroDivisionError:
+        return "Division by zero is not allowed."
+
+print("Simple Calculator")
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+operation = input("Enter your required operation (addition, subtraction, multiplication, division): ")
+
+if operation.lower() == "addition":
+    result = addition(first_number, second_number)
+elif operation.lower() == "subtraction":
+    result = subtraction(first_number, second_number)
+elif operation.lower() == "multiplication":
+    result = multiplication(first_number, second_number)
+elif operation.lower() == "division":
+    division_type = input("Enter division type (int or floor): ")
+    result = division(first_number, second_number, division_type)
+else:
+    result = "Invalid operation"
+
+print(f"Your output of {operation} on {first_number} and {second_number} is {result}")
